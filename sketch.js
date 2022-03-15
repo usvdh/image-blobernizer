af22deeef5dc6f9237e40eabb0e88d103262b3d5
Sen Ueda <sen.ueda@icloud.com>
2022-03-15 13:19:54 +0900

    Remove unused code

--- sketch.js
+++ sketch.js
@@ -2,72 +2,18 @@ let img;
 let imgW, imgH;
 
 function preload() {
-  img = loadImage('./image.jpg')
-  img.loadPixels()
+  img = loadImage("./image.jpg");
+  img.loadPixels();
 }
 
 function setup() {
-  // canvas = createCanvas(700, 400);
-  // canvas.drop(gotFile);
-  console.log(img.height, img.width)
-  canvas = createCanvas(img.width, img.height)
-  // resizeCanvas(imgW, imgH)
+  console.log(img.height, img.width);
+  canvas = createCanvas(img.width, img.height);
 }
 
 function draw() {
-  //background(0);
-  
-  //
-  // if (img) {
-  //   imgUpdate();
-  // }
-  //debug
-   fill(255);
-  // text(imgW + ", " + imgH, 10, 10);
-
-  // if (img.width === img.height) {
-  //   imgW = 400;
-  //   imgH = 400;
-  // } else if (img.width > img.height) {
-  //   imgW = 400;
-  //   imgH = (400 / img.width) * img.height;
-  // } else if (img.width < img.height) {
-  //   imgH = 400;
-  //   imgW = (400 / img.height) * img.width;
-  // }
-  //imgUpdate();
-
-  //console.log(img);
-
-  //img.resize(imgW, imgH);
-  ;
-  lineBlob()
-}
-
-async function gotFile(file) {
-  if (file.type === "image") {
-    img = await loadImage(file.data); //.hide();
-    img.loadPixels();
-
-    console.log(img.canvas.height);
-  } else {
-    alert("You can only upload pictures.");
-  }
-  //createP("Uploaded");
-}
-
-function imgUpdate() {
-  image(img, 0, 0);
-  if (img.width === img.height) {
-    imgW = 400;
-    imgH = 400;
-  } else if (img.width > img.height) {
-    imgW = 400;
-    imgH = (400 / img.width) * img.height;
-  } else if (img.width < img.height) {
-    imgH = 400;
-    imgW = (400 / img.height) * img.width;
-  }
+  fill(255);
+  lineBlob();
 }
 
 function lineBlob() {
